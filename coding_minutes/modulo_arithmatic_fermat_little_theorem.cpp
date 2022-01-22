b723cb10858b7b2aa2d355af536c1820991125ca
ArchanaPrabhu <prabhuarc27@gmail.com>
2022-01-22 17:43:04 +0530

    p12 update

--- coding_minutes/modulo_arithmatic_fermat_little_theorem.cpp
+++ coding_minutes/modulo_arithmatic_fermat_little_theorem.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int fact[1000];
+
 // fast exponentiation
 int powr(int x, int y) {
 	int res = 1;
@@ -25,10 +27,29 @@ int mulm(int x, int y) {
 }
 
 int divm(int x, int y) {
-	return mulm(x, powr(y, p-2));
+	return mulm(x, powr(y, P-2));
+}
+
+int inv(int x) {
+	return powrm(x, P-2);
+}
+
+int ncr(int n, int r) {
+	return mulm(mulm(fact[n], inv(fact[r])), inv(fact[n-r]));
+}
+
+int npr(int n, int r) {
+	return mulm(fact[n], inv(fact[n-r]));
+}
+
+int calculate_factorials() {
+	fact[0] = 1;
+	for (int i = 1;i<N;i++) {
+		fact[i] = mulm(fact[i-1], i);
+	}
 }
 
 int main() {
-
+	calculate_factorials()
 	cout << addm(3, 5)<< endl;
 }
