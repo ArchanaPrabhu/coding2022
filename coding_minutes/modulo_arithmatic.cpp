e20b0de89972bb8751d8ceb96b994f1886e47057
ArchanaPrabhu <prabhuarc27@gmail.com>
2022-01-22 11:24:23 +0530

    p12

---
+++ coding_minutes/modulo_arithmatic.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int powr(int x, int y) {
+	int res = 1;
+
+	while (y) {
+		if (y & 1) { s = mulm(res, x); }
+		y/=2;
+		y = mulm(x, x);
+	}
+}
+
+int addm(int x, int y) {
+	return (x + y) % P;
+}
+
+int subm(int x, int y) {
+	return ((x - y) % P + P) % P;
+}
+
+int mulm(int x, int y) {
+	return (x * y) % P;
+}
+
+int divm(int x, int y) {
+	return mulm(x, powr(y, p-2));
+}
+
+int main() {
+
+	cout << addm(3, 5)<< endl;
+}
