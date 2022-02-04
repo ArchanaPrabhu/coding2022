801381dfc24d39991d860924f0e53f45899678c8
ArchanaPrabhu <prabhuarc27@gmail.com>
2022-02-04 10:25:41 +0530

    p33

---
+++ coding_minutes/generate_parenthesis_luv.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<string> valid;
+
+void generate(string &s, int open, int close) {
+	if (open == 0 and close == 0) {
+		valid.push_back(s);
+		return;
+	}
+
+	if (open > 0) {
+		s.push_back('(');
+		generate(s, open - 1, close);
+		s.pop_back();
+	} 
+
+	if (close > 0) {
+		if (open < close) {
+			s.push_back(')');
+			generate(s, open, close -1);
+			s.pop_back();
+		}
+	}
+
+}
