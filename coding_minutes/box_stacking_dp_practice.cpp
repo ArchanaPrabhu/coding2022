02d0c89df7eb0b988cbf22910d9767fee004fc9d
ArchanaPrabhu <prabhuarc27@gmail.com>
2022-03-09 00:04:54 +0530

    p53

--- coding_minutes/box_stacking_dp_practice.cpp
+++ coding_minutes/box_stacking_dp_practice.cpp
@@ -9,18 +9,40 @@ bool compareBoxes(vector<int> b1, vector<int> b2) {
 	return b1[2] < b2[2];
 }
 
-int boxStacking(vector<vector<int>> boxes) {
-
-sort(boxes.begin()m bixes.end(), compareBoxes);
-
-vector<int> dp(n+1, 0);
-
-for (int i=0;i<n;i++) {
-	dp[i] = boxes[i][2];
+bool canPlace(vector<int> b1, vector<int> b2) {
+	if b1[0]>b2[0] and b1[1]>b2[1] and b1[2] > b2[2] {
+		return true;
+	}
+	return false;
 }
 
+int boxStacking(vector<vector<int>> boxes) {
+	int n = boxes.size();
+	sort(boxes.begin(), boxes.end(), compareBoxes);
 
+	vector<int> dp(n+1, 0);
 
+	for (int i=0;i<n;i++) {
+		dp[i] = boxes[i][2];
+	}
+
+	for (int i=0;i<n;i++) {
+		for (int j=0;j<i;j++) {
+			if (canPlace(boxes[j], boxes[i])) {
+				int current_height = boxes[i][2];
+
+				if(dp[j] + current_height > dp[i]) {
+					dp[i] = dp[j] + current_height;
+				}
+			}
+		}
+	}
+	int max_height = 0;
+	for(int i=0;i<n;i++) {
+		max_height = max(max_height, dp[i]);
+	}
+
+	return max_height;
 }
 
 int main() {
